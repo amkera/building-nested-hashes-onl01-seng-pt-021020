88c941a9b53ed967fe09b092fe96a6d7b8f8711d
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-19 19:08:00 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -9,7 +9,8 @@ def third_challenge
         :age => "54"
       :hero => {
         :name => "Romeo",
-        
+        :age => "15", 
+        :status => "alive"
       :hero_friends => []
    }, 
    :capulet => {
